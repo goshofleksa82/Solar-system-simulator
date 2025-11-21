69df331a3e0e65ad9fa4cd4c4f8a749031bade04
140425123 <nikolownikola06@gmail.com>
2025-11-21 15:02:40 +0200

    center the rectangle

--- SDL demo/main.c
+++ SDL demo/main.c
@@ -1,5 +1,5 @@
-#include <stdio.h>
 #include <SDL3/SDL.h>
+#include <stdio.h>
 
 #define WIDTH 900
 #define HEIGHT 600
@@ -7,23 +7,39 @@
 int main(int argc, char *argv[]) {
     SDL_Init(SDL_INIT_VIDEO);
 
-    SDL_Window* window = SDL_CreateWindow("Bouncy Ball", WIDTH, HEIGHT, 0);
+    SDL_Window* window = SDL_CreateWindow("Centered Rectangle", WIDTH, HEIGHT, SDL_WINDOW_RESIZABLE);
     SDL_Renderer* renderer = SDL_CreateRenderer(window, NULL);
 
-    // Set background to black
-    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-    SDL_RenderClear(renderer);
+    int running = 1;
+    SDL_Event e;
 
-    // SDL3 uses SDL_FRect
-    SDL_FRect rect = {300.0f, 300.0f, 300.0f, 300.0f};
+    SDL_FRect rect = {0, 0, 200.0f, 200.0f}; // size only, position updated each frame
 
-    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255); // red
-    SDL_RenderFillRect(renderer, &rect);
+    while (running) {
+        while (SDL_PollEvent(&e)) {
+            if (e.type == SDL_EVENT_QUIT)
+                running = 0;
+        }
 
-    SDL_RenderPresent(renderer);
-    SDL_Delay(3000);
+        // Get window size
+        int winW, winH;
+        SDL_GetWindowSize(window, &winW, &winH);
+
+        // Center the rectangle
+        rect.x = (winW - rect.w) / 2.0f;
+        rect.y = (winH - rect.h) / 2.0f;
+
+        // Draw
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+        SDL_RenderClear(renderer);
+
+        SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+        SDL_RenderFillRect(renderer, &rect);
+
+        SDL_RenderPresent(renderer);
+        SDL_Delay(16); // ~60 FPS
+    }
 
     SDL_Quit();
     return 0;
 }
-
