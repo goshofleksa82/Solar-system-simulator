ed6c9cacd26390c3864f70886dc7fdc70ab4547d
140425123 <nikolownikola06@gmail.com>
2025-11-21 14:47:01 +0200

    generating a red rectangle

--- SDL demo/main.c
+++ SDL demo/main.c
@@ -1,11 +1,29 @@
 #include <stdio.h>
-#include<SDL3/SDL.h>
+#include <SDL3/SDL.h>
 
 #define WIDTH 900
 #define HEIGHT 600
 
 int main(int argc, char *argv[]) {
-    printf("Hello Bouncy Ball\n");
     SDL_Init(SDL_INIT_VIDEO);
-    SDL_CreateWindow("Bpuncy Ball", WIDTH, HEIGHT, 0);
+
+    SDL_Window* window = SDL_CreateWindow("Bouncy Ball", WIDTH, HEIGHT, 0);
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, NULL);
+
+    // Set background to black
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+    SDL_RenderClear(renderer);
+
+    // SDL3 uses SDL_FRect
+    SDL_FRect rect = {300.0f, 300.0f, 300.0f, 300.0f};
+
+    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255); // red
+    SDL_RenderFillRect(renderer, &rect);
+
+    SDL_RenderPresent(renderer);
+    SDL_Delay(3000);
+
+    SDL_Quit();
+    return 0;
 }
+
