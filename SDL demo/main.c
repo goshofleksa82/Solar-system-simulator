45c516a6635d5055e67700e2f79b5a9378fdec1f
140425123 <nikolownikola06@gmail.com>
2025-11-21 15:48:49 +0200

    Creating a circle

--- SDL demo/main.c
+++ SDL demo/main.c
@@ -4,32 +4,37 @@
 #define WIDTH 900
 #define HEIGHT 600
 
-struct Circle
-{
-    int x;
-    int y;
-    int radius;
+struct Circle {
+    float x;
+    float y;
+    float radius;
 };
 
-
-void FillCircle(SDL_Surface* surface, struct Circle circle)
+void DrawFillCircle(SDL_Renderer *renderer, float cx, float cy, float radius)
 {
-
+    for (float w = -radius; w <= radius; w++) {
+        for (float h = -radius; h <= radius; h++) {
+            if (w*w + h*h <= radius*radius) {
+                SDL_RenderPoint(renderer, cx + w, cy + h);
+            }
+        }
+    }
 }
 
-int main(int argc, char *argv[]) 
+int main(int argc, char *argv[])
 {
     SDL_Init(SDL_INIT_VIDEO);
 
-    SDL_Window* window = SDL_CreateWindow("Centered Rectangle", WIDTH, HEIGHT, SDL_WINDOW_RESIZABLE);
-    SDL_Renderer* renderer = SDL_CreateRenderer(window, NULL);
+    SDL_Window *window = SDL_CreateWindow("Centered Circle", WIDTH, HEIGHT, SDL_WINDOW_RESIZABLE);
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, NULL);
 
     int running = 1;
     SDL_Event e;
 
-    SDL_FRect rect = {0, 0, 200.0f, 200.0f}; // size only, position updated each frame
+    struct Circle circle = {0, 0, 100.0f};
 
-    while (running) {
+    while (running)
+    {
         while (SDL_PollEvent(&e)) {
             if (e.type == SDL_EVENT_QUIT)
                 running = 0;
@@ -39,19 +44,19 @@ int main(int argc, char *argv[])
         int winW, winH;
         SDL_GetWindowSize(window, &winW, &winH);
 
-        // Center the rectangle
-        rect.x = (winW - rect.w) / 2.0f;
-        rect.y = (winH - rect.h) / 2.0f;
+        // center the circle
+        circle.x = winW / 2.0f;
+        circle.y = winH / 2.0f;
 
         // Draw
-        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255); // background black
         SDL_RenderClear(renderer);
 
-        SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
-        SDL_RenderFillRect(renderer, &rect);
+        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255); // white circle
+        DrawFillCircle(renderer, circle.x, circle.y, circle.radius);
 
         SDL_RenderPresent(renderer);
-        SDL_Delay(16); // ~60 FPS
+        SDL_Delay(16);
     }
 
     SDL_Quit();
