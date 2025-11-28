e40a2a7125a8d0a899abf30b45656cc60b6e5d68
140425123 <nikolownikola06@gmail.com>
2025-11-28 15:36:35 +0200

    creating 2D animation of the simulator withe one planet

--- SDL demo/main.c
+++ SDL demo/main.c
@@ -1,5 +1,6 @@
 #include <SDL3/SDL.h>
 #include <stdio.h>
+#include <math.h>
 
 #define WIDTH 900
 #define HEIGHT 600
@@ -8,8 +9,10 @@ struct Circle {
     float x;
     float y;
     float radius;
+    Uint8 r, g, b;
 };
 
+// ----- Draw Filled Circle -----
 void DrawFillCircle(SDL_Renderer *renderer, float cx, float cy, float radius)
 {
     for (float w = -radius; w <= radius; w++) {
@@ -21,17 +24,55 @@ void DrawFillCircle(SDL_Renderer *renderer, float cx, float cy, float radius)
     }
 }
 
+// ----- Draw NOT Filled Circle (Orbit) -----
+void DrawCircle(SDL_Renderer *renderer, float cx, float cy, float radius)
+{
+    float x = radius;
+    float y = 0;
+    float decision = 1 - x;
+
+    while (y <= x)
+    {
+        SDL_RenderPoint(renderer, cx + x, cy + y);
+        SDL_RenderPoint(renderer, cx + y, cy + x);
+        SDL_RenderPoint(renderer, cx - y, cy + x);
+        SDL_RenderPoint(renderer, cx - x, cy + y);
+        SDL_RenderPoint(renderer, cx - x, cy - y);
+        SDL_RenderPoint(renderer, cx - y, cy - x);
+        SDL_RenderPoint(renderer, cx + y, cy - x);
+        SDL_RenderPoint(renderer, cx + x, cy - y);
+
+        y++;
+
+        if (decision <= 0)
+            decision += 2 * y + 1;
+        else {
+            x--;
+            decision += 2 * (y - x) + 1;
+        }
+    }
+}
+
 int main(int argc, char *argv[])
 {
     SDL_Init(SDL_INIT_VIDEO);
 
-    SDL_Window *window = SDL_CreateWindow("Centered Circle", WIDTH, HEIGHT, SDL_WINDOW_RESIZABLE);
+    SDL_Window *window = SDL_CreateWindow("Orbiting Planet", WIDTH, HEIGHT, SDL_WINDOW_RESIZABLE);
     SDL_Renderer *renderer = SDL_CreateRenderer(window, NULL);
 
     int running = 1;
     SDL_Event e;
 
-    struct Circle circle = {0, 0, 100.0f};
+    // STAR (center)
+    struct Circle star = {0, 0, 20.0f, 255, 255, 0}; // yellow
+
+    // PLANET
+    struct Circle planet = {0, 0, 10.0f, 0, 150, 255}; // blue
+
+    // Orbit settings
+    float orbit_radius = 150.0f;
+    float angle = 0.0f;          // current angle
+    float angular_speed = 0.03f; // orbit speed
 
     while (running)
     {
@@ -44,19 +85,33 @@ int main(int argc, char *argv[])
         int winW, winH;
         SDL_GetWindowSize(window, &winW, &winH);
 
-        // center the circle
-        circle.x = winW / 2.0f;
-        circle.y = winH / 2.0f;
+        // Center the star
+        star.x = winW / 2.0f;
+        star.y = winH / 2.0f;
 
-        // Draw
-        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255); // background black
+        // Move the planet along a circular orbit
+        angle += angular_speed;
+        planet.x = star.x + cosf(angle) * orbit_radius;
+        planet.y = star.y + sinf(angle) * orbit_radius;
+
+        // ---- DRAWING ----
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255); // black background
         SDL_RenderClear(renderer);
 
-        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255); // white circle
-        DrawFillCircle(renderer, circle.x, circle.y, circle.radius);
+        // Draw orbit
+        SDL_SetRenderDrawColor(renderer, 120, 120, 120, 255); 
+        DrawCircle(renderer, star.x, star.y, orbit_radius);
+
+        // Draw star
+        SDL_SetRenderDrawColor(renderer, star.r, star.g, star.b, 255);
+        DrawFillCircle(renderer, star.x, star.y, star.radius);
+
+        // Draw planet
+        SDL_SetRenderDrawColor(renderer, planet.r, planet.g, planet.b, 255);
+        DrawFillCircle(renderer, planet.x, planet.y, planet.radius);
 
         SDL_RenderPresent(renderer);
-        SDL_Delay(16);
+        SDL_Delay(16); // ~60 FPS
     }
 
     SDL_Quit();
